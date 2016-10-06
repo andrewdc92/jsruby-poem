ae85954734e0956e7373b3ac6f552f9e4d8b08f8
Andrew Cordivari <andrewcord@williamvarismbp.gateway.sonic.net>
2016-10-05 22:14:26 -0700

    adding to my repo, did not fork from travis

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,13 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require bootstrap
+//= require title
+//= require roses_are_red
+//= require violets_are_blue
+//= require compile_your_assets
+//= require then_hit_opt_cmd_u
+//= require poem_meta_data
